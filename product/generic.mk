10045f7429a4397a94bc6640c9dd82389e64d81c
Roman Kiryanov <rkir@google.com>
2024-10-16 22:46:35 +0000

    Put apns-conf.xml into /data/misc/apns
    the data partition must be empty, we can't
    populate it during built time.
    
    Bug: 372557166
    Test: presubmit
    Change-Id: Icfc363921e0b6965755347505df6a7ccd55d0396
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- product/generic.mk
+++ product/generic.mk
@@ -126,7 +126,7 @@ DISABLE_RILD_OEM_HOOK := true
 $(call soong_config_set,ril,disable_rild_oem_hook,true)
 
 PRODUCT_COPY_FILES += \
-    device/generic/goldfish/data/etc/apns-conf.xml:data/misc/apns/apns-conf.xml \
+    device/generic/goldfish/data/etc/apns-conf.xml:$(TARGET_COPY_OUT_VENDOR)/etc/apns/apns-conf.xml \
     device/generic/goldfish/radio/EmulatorRadioConfig/radioconfig.xml:data/misc/emulator/config/radioconfig.xml \
     device/generic/goldfish/data/etc/iccprofile_for_sim0.xml:data/misc/modem_simulator/iccprofile_for_sim0.xml \
     device/google/cuttlefish/host/commands/modem_simulator/files/iccprofile_for_sim0.xml:data/misc/modem_simulator/iccprofile_for_sim_tel_alaska.xml \
